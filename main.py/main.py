b29e4213d3fb4fce8ee235c82c97bf77e01d5f0c
avi <avi10137@gmail.com>
2026-05-20 10:08:18 +0300

    fix project structure

---
+++ main.py/main.py
@@ -0,0 +1,48 @@
+def menu()->None:
+    """the function shows the user
+    the possibilities based on the number recive 
+    input from user 
+    and if the number is not found print typeerror
+     and if the input is incorrect displays it valueeror"""
+    pass
+def handle_add_solider()->None:
+    """"the function creat a solider ask the user name and id 
+    checks the name if incorrect return
+    valueror calls the function that cheks 
+     if the solider exist if the solider exsist return
+      an eror massage if not issues a message that the soldier has been added """
+    pass
+def add_solider(id,name,list):
+    """Checks if the soldier exists, if so returns an error, if not adds to the list"""
+    pass
+def sub_solider(id,name,list):
+    """The function is supposed to subtract a soldier from the list. The function checks if the soldier exists, if not
+    , returns an error, if so, subtracts from the list and returns it"""
+    pass
+def is_solider(id, name):
+    """The function checks if the soldier
+      exists in the system and returns a Boolean"""
+    pass
+def handle_duty():
+    """The function receives a date
+      day and an id to create a task
+      calls the add shift function"""
+    pass
+def add_duty(id, day,duty):
+    """The function receives a day of duty and an id and a dictionary, calls a day test 
+    function and a duty test function, if returned, updates a dictionary."""
+    pass
+def is_day():
+    """The function accepts a day 
+    and a list of possible days and returns a boolean"""
+    pass
+def is_duty(duty):
+    """The function checks whether the soldier is on
+      duty and returns a boolean"""
+    pass
+
+
+
+
+
+
